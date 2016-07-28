2f843e43516c270af89f87960353d2cac58ada68
zyp-git <zhangyunpeng@dakedaojia.com>
2016-07-28 19:19:07 +0800

    app socket

--- shopForiOS/shopForiOS/AppDelegate.m
+++ shopForiOS/shopForiOS/AppDelegate.m
@@ -11,7 +11,11 @@
 #import "searchOrderVC.h"
 #import "shopManageVC.h"
 #import "baseNavigationController.h"
-@interface AppDelegate ()
+#import "RHWebSocketChannel.h"
+
+@interface AppDelegate () <SRWebSocketDelegate>
+
+@property (nonatomic, strong) RHWebSocketChannel *webSocketChannel;
 
 @end
 
@@ -30,6 +34,10 @@
     
     //在显示窗口子视图加入ViewController视图
     //[window addSubview:tabBarController.view]
+    
+    _webSocketChannel = [[RHWebSocketChannel alloc] initWithURL:@"http://122.114.94.150:8888"];
+    _webSocketChannel.delegate = self;
+    [_webSocketChannel openConnection];
     return YES;
 }
 -(void)setTabBarController{
@@ -55,6 +63,34 @@
     bgView.backgroundColor = [UIColor whiteColor];
     [self.tabBarController.tabBar insertSubview:bgView atIndex:0];
     self.tabBarController.tabBar.opaque = YES;
+}
+#pragma mark - SRWebSocketDelegate
+
+// message will either be an NSString if the server is using text
+// or NSData if the server is using binary.
+- (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message
+{
+    NSLog(@"Received: %@", message);
+}
+
+- (void)webSocketDidOpen:(SRWebSocket *)webSocket
+{
+    NSLog(@"Websocket Connected ...");
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didFailWithError:(NSError *)error
+{
+    NSLog(@":( Websocket Failed With Error %@", error);
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean
+{
+    NSLog(@"WebSocket closed: code-[%ld], reason-[%@]", code, reason);
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didReceivePong:(NSData *)pongPayload
+{
+    NSLog(@"Websocket received pong");
 }
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
